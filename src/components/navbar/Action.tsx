5313a2bef30381a68aa0dff16a9fb79507900bd4
lucianocolin <lucianocolin99@gmail.com>
2025-02-26 17:42:09 -0300

    Modify sign in button with new methods

--- src/components/navbar/Action.tsx
+++ src/components/navbar/Action.tsx
@@ -1,13 +1,46 @@
 import Button from './Button';
 
+import { useWallet } from '@/hooks/auth/useWallet';
+
 type PropTypes = {
-	connected: boolean;
+	readonly connected: boolean;
 };
+
 export default function Action({ connected }: PropTypes) {
+	const { isLoading, connectWallet, signInWithTransaction, publicKey } =
+		useWallet();
+
 	if (connected) {
 		return (
 			<Button data-test="sign-out" to="/auth/sign-out" innerText="Sign Out" />
 		);
 	}
-	return <Button data-test="sign-in" to="/auth/sign-in" innerText="Sign In" />;
+
+	if (!publicKey) {
+		return (
+			<button
+				className="flex items-center justify-center rounded-full bg-blue-600 font-bold text-white m-1 px-4 shadow h-10 hover:bg-blue-500"
+				onClick={() => connectWallet()}
+				data-test="connect-wallet-btn"
+			>
+				Connect your Wallet
+			</button>
+		);
+	}
+
+	return isLoading ? (
+		<button className="bg-blue-500 text-white font-bold rounded-full min-w-[100px]">
+			<span className="material-symbols-outlined animate-spin pointer-events-none align-middle">
+				progress_activity
+			</span>
+		</button>
+	) : (
+		<button
+			className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded-full"
+			onClick={() => signInWithTransaction(publicKey)}
+			data-test="sign-in-btn"
+		>
+			Sign In
+		</button>
+	);
 }
