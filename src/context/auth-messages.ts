444748a55fdf519e23073d8fc442ef1af79d795f
lucianocolin <lucianocolin99@gmail.com>
2025-02-26 17:42:37 -0300

    Add new success and error messages

--- src/context/auth-messages.ts
+++ src/context/auth-messages.ts
@@ -6,3 +6,9 @@ export const CONFIRMATION_SENT_MESSAGE =
 export const SESSION_EXPIRED_ERROR =
 	'Your session has expired.\nPlease sign in again.';
 export const SIGN_OUT_SUCCESS_MESSAGE = 'You have successfully signed out.';
+export const CONNECT_WALLET_ERROR = 'There was an error connecting your wallet';
+export const INVALID_TRANSACTION_ERROR = 'Invalid transaction data';
+export const SIGN_TRANSACTION_ERROR =
+	'There was an error signing the transaction';
+export const TRANSACTION_SIGNED_MESSAGE = 'Transaction signed successfully';
+export const CONNECT_WALLET_MESSAGE = 'Wallet connected successfully';
