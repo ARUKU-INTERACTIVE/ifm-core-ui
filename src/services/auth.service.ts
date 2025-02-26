23dc8930a7c0f6c561ee8b51d91d3431aee6875d
lucianocolin <lucianocolin99@gmail.com>
2025-02-26 17:43:40 -0300

    Add get challenge query and modify sign in one

--- src/services/auth.service.ts
+++ src/services/auth.service.ts
@@ -1,6 +1,7 @@
 import { ApiRequestConfig, apiService } from './api.service';
 
 import { ISingleResponse } from '@/interfaces/api/IApiBaseResponse';
+import { IChallengeTransactionResponse } from '@/interfaces/auth/IChallengeTransactionResponse';
 import { IRefreshSessionResponse } from '@/interfaces/auth/IRefreshSessionResponse';
 import { ISignInResponse } from '@/interfaces/auth/ISignInResponse';
 import { ISignUpResponse } from '@/interfaces/auth/ISignUpResponse';
@@ -13,12 +14,12 @@ class AuthService implements IAuthService {
 	constructor(api: IApiService<ApiRequestConfig>) {
 		this.api = api;
 	}
-	async signIn(username: string, password: string, config?: ApiRequestConfig) {
-		return await this.api.post<ISingleResponse<ISignInResponse>>(
-			'/auth/sign-in',
-			{ username, password },
-			config,
-		);
+	async signIn(transactionSigned: string, publicKey: string, nonce: string) {
+		return await this.api.post<ISignInResponse>('/auth/sign-in', {
+			transactionSigned,
+			publicKey,
+			nonce,
+		});
 	}
 	async signUp(username: string, password: string, config?: ApiRequestConfig) {
 		return await this.api.post<ISingleResponse<ISignUpResponse>>(
@@ -62,6 +63,19 @@ class AuthService implements IAuthService {
 			{ username, refreshToken },
 			config,
 		);
+	}
+	async getChallengeTransaction(publicKey: string) {
+		try {
+			const queryParam = `?publicKey=${publicKey}`;
+
+			return await this.api.get<IChallengeTransactionResponse>(
+				`/auth/challenge${queryParam}`,
+			);
+		} catch (error) {
+			const err = error as Error;
+			console.error('[GET_CHALLENGE_TRANSACTION_ERROR]', err);
+			throw new Error(err.message);
+		}
 	}
 }
 
