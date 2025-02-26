c6e484fec0dc2f7bfa35c9492d9069d1661bbdff
lucianocolin <lucianocolin99@gmail.com>
2025-02-26 17:20:14 -0300

    Add simple signer hook

---
+++ src/hooks/auth/useSimpleSigner.tsx
@@ -0,0 +1,46 @@
+import { Keypair } from '@stellar/stellar-sdk';
+import { useState } from 'react';
+import { connectWallet, signTransaction } from 'simple-stellar-signer-api';
+
+export const useSimpleSigner = () => {
+	const [publicKey, setPublicKey] = useState<string>('');
+	const network = import.meta.env.VITE_SIMPLE_SIGNER_URL;
+
+	const handleConnectWallet = async () => {
+		try {
+			const { publicKey } = await connectWallet(network);
+
+			if (Keypair.fromPublicKey(publicKey)) {
+				setPublicKey(publicKey);
+			}
+		} catch (error) {
+			const err = error as Error;
+			console.error('[CONNECT_WALLET_ERROR]', err);
+			if (err.message.includes('User cancelled process')) {
+				throw new Error('SIMPLE_SIGNER_CONNECT_CANCELLED');
+			}
+			throw err;
+		}
+	};
+
+	const handleSignTransaction = async (xdr: string, description?: string) => {
+		try {
+			return await signTransaction(xdr, network, {
+				description,
+			});
+		} catch (error) {
+			const err = error as Error;
+			console.error('[SIGN_TRANSACTION_ERROR]', err);
+			if (err.message.includes('User cancelled process')) {
+				throw new Error('SIMPLE_SIGNER_SIGN_CANCELLED');
+			}
+			throw err;
+		}
+	};
+
+	return {
+		publicKey,
+		handleConnectWallet,
+		handleSignTransaction,
+	};
+};
