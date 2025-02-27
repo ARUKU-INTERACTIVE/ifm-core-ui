5d83ce49785bcad16fe22b06c8d2e7c65f971dc5
lucianocolin <lucianocolin99@gmail.com>
2025-02-27 16:57:32 -0300

    Add loading button component

---
+++ src/components/ui/LoadingButton.tsx
@@ -0,0 +1,13 @@
+import React from 'react';
+
+const LoadingButton: React.FC = () => {
+	return (
+		<button className="bg-blue-500 text-white font-bold rounded-full min-w-[100px]">
+			<span className="material-symbols-outlined animate-spin pointer-events-none align-middle">
+				progress_activity
+			</span>
+		</button>
+	);
+};
+
+export default LoadingButton;
