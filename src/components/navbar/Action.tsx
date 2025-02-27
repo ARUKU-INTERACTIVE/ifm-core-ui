d886bbd2672d3c56cbee9832b22748f7c5c2c8f5
lucianocolin <lucianocolin99@gmail.com>
2025-02-27 12:12:57 -0300

    Modify sign in to automatically sign transaction

--- src/components/navbar/Action.tsx
+++ src/components/navbar/Action.tsx
@@ -1,3 +1,5 @@
+import { useEffect } from 'react';
+
 import Button from './Button';
 
 import { useWallet } from '@/hooks/auth/useWallet';
@@ -10,21 +12,15 @@ export default function Action({ connected }: PropTypes) {
 	const { isLoading, connectWallet, signInWithTransaction, publicKey } =
 		useWallet();
 
+	useEffect(() => {
+		if (!publicKey) return;
+
+		signInWithTransaction(publicKey);
+	}, [publicKey]);
+
 	if (connected) {
 		return (
 			<Button data-test="sign-out" to="/auth/sign-out" innerText="Sign Out" />
-		);
-	}
-
-	if (!publicKey) {
-		return (
-			<button
-				className="flex items-center justify-center rounded-full bg-blue-600 font-bold text-white m-1 px-4 shadow h-10 hover:bg-blue-500"
-				onClick={() => connectWallet()}
-				data-test="connect-wallet-btn"
-			>
-				Connect your Wallet
-			</button>
 		);
 	}
 
@@ -37,7 +33,7 @@ export default function Action({ connected }: PropTypes) {
 	) : (
 		<button
 			className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded-full"
-			onClick={() => signInWithTransaction(publicKey)}
+			onClick={() => connectWallet()}
 			data-test="sign-in-btn"
 		>
 			Sign In
