412c1953680095f709d997c87baea0a4e101897c
lucianocolin <lucianocolin99@gmail.com>
2025-02-27 09:24:47 -0300

    Add new auth tests

--- cypress/tests/ui/authentication.spec.ts
+++ cypress/tests/ui/authentication.spec.ts
@@ -1,88 +1,198 @@
 import {
 	CONFIRMATION_SENT_MESSAGE,
+	CONNECT_WALLET_ERROR,
+	CONNECT_WALLET_MESSAGE,
 	SIGN_IN_SUCCESS_MESSAGE,
 	SIGN_OUT_SUCCESS_MESSAGE,
+	SIGN_TRANSACTION_ERROR,
 	SIGN_UP_SUCCESS_MESSAGE,
+	TRANSACTION_SIGNED_MESSAGE,
 } from '@context/auth-messages';
-
-import { PASSWORD_REQUIRED } from '@components/auth/schemas/schema-errors';
 
 describe('/auth', () => {
 	const username = 'example@test.com';
 	const password = 'Supersecret2024~';
 	const code = '123456';
+	const walletAddress =
+		'GCTIUQVU4GWDINAEC3B2E2PMVTW45YQ5MMHKQI737YHCVP6UI4ET4G2R';
+	const simpleSignerUrl = Cypress.env('VITE_SIMPLE_SIGNER_URL');
 
 	describe('/', () => {
-		it('From the navbar, should be able to access the sign-in page', () => {
-			cy.visit('/');
-			cy.clickLinkAndVerifyUrl('sign-in', '/auth/sign-in');
-		});
-	});
-
-	describe('/auth/sign-in', () => {
 		beforeEach(() => {
-			cy.visit('/auth/sign-in');
+			cy.visit('/');
 		});
-		it('Should be able to sign-in', () => {
-			cy.signIn();
+		it('Should be able to connect your wallet', () => {
+			cy.window().then((window) => {
+				cy.stub(window, 'open')
+					.as('connect-wallet')
+					.callsFake(() => null);
+			});
+
+			cy.getBySel('connect-wallet-btn')
+				.should('have.text', 'Connect your Wallet')
+				.click();
+			cy.get('@connect-wallet').should('be.called');
+
+			const connectEvent = new MessageEvent('message', {
+				data: {
+					type: 'onConnect',
+					page: `${simpleSignerUrl}/connect`,
+					message: {
+						publicKey: walletAddress,
+						wallet: 'albedo',
+					},
+				},
+				origin: simpleSignerUrl,
+			});
+
+			cy.window().then((win) => {
+				win.dispatchEvent(connectEvent);
+			});
+
+			cy.getBySel('toast-container').contains(CONNECT_WALLET_MESSAGE);
+		});
+
+		it('should be able to sign in', () => {
+			cy.interceptApi(
+				`/auth/challenge?publicKey=${walletAddress}`,
+				{ method: 'GET' },
+				{ fixture: 'auth/challenge-transaction-response.json' },
+			);
+			cy.interceptApi(
+				'/auth/sign-in',
+				{ method: 'POST' },
+				{ fixture: 'auth/sign-in.json' },
+			).as('sign-in');
+
+			cy.window().then((window) => {
+				cy.stub(window, 'open')
+					.as('connect-wallet')
+					.callsFake(() => null);
+			});
+
+			cy.getBySel('connect-wallet-btn')
+				.should('have.text', 'Connect your Wallet')
+				.click();
+			cy.get('@connect-wallet').should('be.called');
+
+			const connectEvent = new MessageEvent('message', {
+				data: {
+					type: 'onConnect',
+					page: `${simpleSignerUrl}/connect`,
+					message: {
+						publicKey: walletAddress,
+						wallet: 'albedo',
+					},
+				},
+				origin: simpleSignerUrl,
+			});
+
+			cy.window().then((win) => {
+				win.dispatchEvent(connectEvent);
+			});
+
+			cy.getBySel('toast-container').contains(CONNECT_WALLET_MESSAGE);
+
+			cy.getBySel('sign-in-btn').should('have.text', 'Sign In').click();
+
+			const signEvent = new MessageEvent('message', {
+				data: {
+					type: 'onSign',
+					page: `${simpleSignerUrl}/sign`,
+					message: {
+						signedXDR:
+							'AAAAAGrj5kK0Xb3d3c3NvZ2Z3YXJpZ2F0ZQAAAAAAAAAAABiGAAAAQAAAAA=',
+					},
+				},
+				origin: simpleSignerUrl,
+			});
+
+			cy.window().then((win) => {
+				win.dispatchEvent(signEvent);
+			});
+
+			cy.getBySel('toast-container').contains(TRANSACTION_SIGNED_MESSAGE);
 			cy.getBySel('toast-container').contains(SIGN_IN_SUCCESS_MESSAGE);
 		});
 
-		it('Should be able to visit sign-up', () => {
-			cy.clickLinkAndVerifyUrl('link-sign-up', '/auth/sign-up');
-		});
-		it('Should be able to visit forgot password', () => {
-			cy.clickLinkAndVerifyUrl('link-forgot-password', '/auth/forgot-password');
-		});
-		it('Should be able to visit confirm user', () => {
-			cy.clickLinkAndVerifyUrl('link-confirm-user', '/auth/confirm-user');
-		});
-		it('Username field validation', () => {
-			cy.validateUsername('sign-in-username', 'form-input-error-username');
-		});
-		it('Password field validation', () => {
-			cy.getBySel('sign-in-password').focus().blur();
-			cy.getBySel('form-input-error-password').contains(PASSWORD_REQUIRED);
-		});
-		it('If the user does not exist, it should show an error', () => {
-			const errorResponse = {
-				error: {
-					detail: `${username} was not found`,
-					source: { pointer: '/api/v1/auth/sign-in' },
-					status: '404',
-					title: 'Username Not Found',
+		it('should show an error if the wallet is not connected', () => {
+			cy.window().then((window) => {
+				cy.stub(window, 'open')
+					.as('connect-wallet')
+					.callsFake(() => null);
+			});
+
+			cy.getBySel('connect-wallet-btn')
+				.should('have.text', 'Connect your Wallet')
+				.click();
+			cy.get('@connect-wallet').should('be.called');
+
+			const connectCancelEvent = new MessageEvent('message', {
+				data: {
+					type: 'onCancel',
+					page: `${simpleSignerUrl}/connect`,
+					message: 'The user has canceled the operation',
 				},
-			};
-			cy.getInputAndType('sign-in-username', username);
-			cy.getInputAndType('sign-in-password', password);
-			cy.interceptApi(
-				'/auth/sign-in',
-				{ method: 'POST' },
-				{ body: errorResponse, statusCode: +errorResponse.error.status },
-			).as('error-sign-in');
-			cy.getBySel('sign-in-submit').click();
-			cy.wait('@error-sign-in');
-			cy.getBySel('toast-container').contains(errorResponse.error.detail);
+				origin: simpleSignerUrl,
+			});
+
+			cy.window().then((win) => {
+				win.dispatchEvent(connectCancelEvent);
+			});
+
+			cy.getBySel('toast-container').contains(CONNECT_WALLET_ERROR);
 		});
-		it('If the password is incorrect, it should show an error', () => {
-			const errorResponse = {
-				error: {
-					detail: 'Invalid username or password',
-					source: { pointer: '/api/v1/auth/sign-in' },
-					status: '401',
-					title: 'Unauthorized',
-				},
-			};
-			cy.getInputAndType('sign-in-username', username);
-			cy.getInputAndType('sign-in-password', password);
+
+		it('Should show an error if the transaction failed', () => {
 			cy.interceptApi(
-				'/auth/sign-in',
-				{ method: 'POST' },
-				{ body: errorResponse, statusCode: +errorResponse.error.status },
-			).as('error-sign-in');
-			cy.getBySel('sign-in-submit').click();
-			cy.wait('@error-sign-in');
-			cy.getBySel('toast-container').contains(errorResponse.error.detail);
+				`/auth/challenge?publicKey=${walletAddress}`,
+				{ method: 'GET' },
+				{ fixture: 'auth/challenge-transaction-response.json' },
+			);
+
+			cy.window().then((window) => {
+				cy.stub(window, 'open')
+					.as('connect-wallet')
+					.callsFake(() => null);
+			});
+
+			cy.getBySel('connect-wallet-btn')
+				.should('have.text', 'Connect your Wallet')
+				.click();
+			cy.get('@connect-wallet').should('be.called');
+
+			const connectEvent = new MessageEvent('message', {
+				data: {
+					type: 'onConnect',
+					page: `${simpleSignerUrl}/connect`,
+					message: {
+						publicKey: walletAddress,
+						wallet: 'albedo',
+					},
+				},
+				origin: simpleSignerUrl,
+			});
+
+			cy.window().then((win) => {
+				win.dispatchEvent(connectEvent);
+			});
+
+			cy.getBySel('sign-in-btn').should('have.text', 'Sign In').click();
+
+			const signCancelEvent = new MessageEvent('message', {
+				data: {
+					type: 'onCancel',
+					page: `${simpleSignerUrl}/sign`,
+					message: 'The user has canceled the operation',
+				},
+				origin: simpleSignerUrl,
+			});
+
+			cy.window().then((win) => {
+				win.dispatchEvent(signCancelEvent);
+			});
+
+			cy.getBySel('toast-container').contains(SIGN_TRANSACTION_ERROR);
 		});
 	});
 
@@ -102,9 +212,6 @@ describe('/auth', () => {
 			cy.wait('@sign-up');
 			cy.getBySel('toast-container').contains(SIGN_UP_SUCCESS_MESSAGE);
 			cy.getBySel('toast-container').contains(CONFIRMATION_SENT_MESSAGE);
-		});
-		it('Should be able to visit sign-in', () => {
-			cy.clickLinkAndVerifyUrl('link-sign-in', '/auth/sign-in');
 		});
 		it('Username field validation', () => {
 			cy.validateUsername('sign-up-username', 'form-input-error-username');
@@ -172,25 +279,6 @@ describe('/auth', () => {
 				'form-input-error-username',
 			);
 		});
-		it('If the user does not exist, it should show an error', () => {
-			const errorResponse = {
-				error: {
-					detail: `${username} was not found`,
-					source: { pointer: '/api/v1/auth/sign-in' },
-					status: '404',
-					title: 'Username Not Found',
-				},
-			};
-			cy.getInputAndType('forgot-password-username', username);
-			cy.interceptApi(
-				'/auth/forgot-password',
-				{ method: 'POST' },
-				{ body: errorResponse, statusCode: +errorResponse.error.status },
-			).as('error-forgot-password');
-			cy.getBySel('forgot-password-submit').click();
-			cy.wait('@error-forgot-password');
-			cy.getBySel('toast-container').contains(errorResponse.error.detail);
-		});
 	});
 
 	describe('/auth/confirm-user', () => {
@@ -237,34 +325,11 @@ describe('/auth', () => {
 				'/auth/resend-confirmation-code',
 			);
 		});
-		it('Should be able to visit confirm password', () => {
-			cy.clickLinkAndVerifyUrl('link-sign-in', '/auth/sign-in');
-		});
 		it('Username field validation', () => {
 			cy.validateUsername('confirm-user-username', 'form-input-error-username');
 		});
 		it('Code field validation', () => {
 			cy.validateCode('confirm-user-code', 'form-input-error-code');
-		});
-		it('If the user does not exist, it should show an error', () => {
-			const errorResponse = {
-				error: {
-					detail: `${username} was not found`,
-					source: { pointer: '/api/v1/auth/sign-in' },
-					status: '404',
-					title: 'Username Not Found',
-				},
-			};
-			cy.interceptApi(
-				'/auth/confirm-user',
-				{ method: 'POST' },
-				{ body: errorResponse, statusCode: +errorResponse.error.status },
-			).as('error-confirm-user');
-			cy.getInputAndType('confirm-user-username', username);
-			cy.getInputAndType('confirm-user-code', code);
-			cy.getBySel('confirm-user-submit').click();
-			cy.wait('@error-confirm-user');
-			cy.getBySel('toast-container').contains(errorResponse.error.detail);
 		});
 		it('If the code is invalid, it should show an error', () => {
 			const errorResponse = {
@@ -319,9 +384,6 @@ describe('/auth', () => {
 		it('Should be able to visit forgot password', () => {
 			cy.clickLinkAndVerifyUrl('link-forgot-password', '/auth/forgot-password');
 		});
-		it('Should be able to visit sign in', () => {
-			cy.clickLinkAndVerifyUrl('link-sign-in', '/auth/sign-in');
-		});
 		it('Username field validation', () => {
 			cy.validateUsername(
 				'confirm-password-username',
@@ -337,27 +399,7 @@ describe('/auth', () => {
 		it('Code field validation', () => {
 			cy.validateCode('confirm-password-code', 'form-input-error-code');
 		});
-		it('If the user does not exist, it should show an error', () => {
-			const errorResponse = {
-				error: {
-					detail: `${username} was not found`,
-					source: { pointer: '/api/v1/auth/sign-in' },
-					status: '404',
-					title: 'Username Not Found',
-				},
-			};
-			cy.interceptApi(
-				'/auth/confirm-password',
-				{ method: 'POST' },
-				{ body: errorResponse, statusCode: +errorResponse.error.status },
-			).as('error-confirm-password');
-			cy.getInputAndType('confirm-password-username', username);
-			cy.getInputAndType('confirm-password-password', password);
-			cy.getInputAndType('confirm-password-code', code);
-			cy.getBySel('confirm-password-submit').click();
-			cy.wait('@error-confirm-password');
-			cy.getBySel('toast-container').contains(errorResponse.error.detail);
-		});
+
 		it('If the code is invalid, it should show an error', () => {
 			const errorResponse = {
 				error: {
