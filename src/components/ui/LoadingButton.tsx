49975d2dde568df8e3016fd2e06c4d805ee84b2b
lucianocolin <lucianocolin99@gmail.com>
2025-03-06 10:58:30 -0300

    Delete unnecessary 'React' import

--- src/components/ui/LoadingButton.tsx
+++ src/components/ui/LoadingButton.tsx
@@ -1,6 +1,4 @@
-import React from 'react';
-
-const LoadingButton: React.FC = () => {
+const LoadingButton = () => {
 	return (
 		<button className="bg-blue-500 text-white font-bold rounded-full min-w-[100px]">
 			<span className="material-symbols-outlined animate-spin pointer-events-none align-middle">
