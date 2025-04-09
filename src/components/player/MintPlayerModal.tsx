5a379ebd4b4ae8561504d371ff9cd66a058c4c9e
lucianocolin <lucianocolin99@gmail.com>
2025-04-08 21:41:07 -0300

    Add mint player modal

---
+++ src/components/player/MintPlayerModal.tsx
@@ -0,0 +1,26 @@
+import MintPlayerForm from './MintPlayerForm';
+
+type Props = {
+	isOpen: boolean;
+	onHide: () => void;
+};
+
+const MintPlayerModal = ({ isOpen, onHide }: Props) => {
+	if (!isOpen) return null;
+
+	return (
+		<div className="fixed inset-0 bg-black bg-opacity-50 flex justify-center items-center">
+			<div className="bg-white p-6 rounded-md w-[80%] max-w-md">
+				<h2
+					className="text-2xl font-bold text-center mb-4"
+					data-test="mint-player-modal-title"
+				>
+					Mint Player
+				</h2>
+				<MintPlayerForm onHide={onHide} />
+			</div>
+		</div>
+	);
+};
+
+export default MintPlayerModal;
