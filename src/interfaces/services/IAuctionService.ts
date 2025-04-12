8ffa4390b10f8a1e366859029adb3697a63ebb2b
lucianocolin <lucianocolin99@gmail.com>
2025-04-12 13:57:54 -0300

    Add auctions methods in auction service

---
+++ src/interfaces/services/IAuctionService.ts
@@ -0,0 +1,16 @@
+import { IListResponse, ISingleResponse } from '../api/IApiBaseResponse';
+import { ITransactionResponse } from '../api/ITransactionResponse';
+import { IAuction } from '../auction/IAuction';
+import { ICreateAuctionTransactionParams } from '../auction/ICreateAuctionTransaction';
+import { ISubmitCreateAuctionTransactionParams } from '../auction/ISubmitCreateAuction';
+
+export interface IAuctionService {
+	getAll(): Promise<IListResponse<IAuction>>;
+	createAuctionTransaction(
+		params: ICreateAuctionTransactionParams,
+	): Promise<ISingleResponse<ITransactionResponse>>;
+
+	submitCreateAuctionTransaction(
+		params: ISubmitCreateAuctionTransactionParams,
+	): Promise<ISingleResponse<IAuction>>;
+}
