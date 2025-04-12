4cf653beb7282c68e97faf18decdda737b26a66d
lucianocolin <lucianocolin99@gmail.com>
2025-04-12 14:04:03 -0300

    Add player and auction messages

---
+++ src/components/player/player-messages.ts
@@ -0,0 +1,4 @@
+export const SUBMIT_MINT_PLAYER_SAC_SUCCESS_MESSAGE =
+	'Player address added successfully';
+export const SUBMIT_MINT_PLAYER_SAC_ERROR_MESSAGE =
+	"There was a problem at adding player's address";
---
+++ src/interfaces/auction/auction-messages.ts
@@ -0,0 +1,4 @@
+export const SUBMIT_CREATE_AUCTION_SUCCESS_MESSAGE =
+	'Auction submitted successfully';
+export const SUBMIT_CREATE_AUCTION_ERROR_MESSAGE =
+	'There was a problem at submitting the auction';
