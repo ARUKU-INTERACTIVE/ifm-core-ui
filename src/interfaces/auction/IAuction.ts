b3ea57e9b7ef84563a126123aac8f1d9f2f9f49c
lucianocolin <lucianocolin99@gmail.com>
2025-04-12 13:58:49 -0300

    Modify Auction interface to add new props

--- src/interfaces/auction/IAuction.ts
+++ src/interfaces/auction/IAuction.ts
@@ -1,9 +1,19 @@
 import { IPlayer } from '../player/IPlayer';
 
 export interface IAuction {
+	id: number;
+	uuid: string;
 	externalId: number;
+	highestBidAmount: number;
+	endTime: number;
+	startTime: number;
 	player: IPlayer;
 	status: AuctionStatus;
+	highestBidderAddress: string;
+	playerAddress: string;
+	createdAt: string;
+	updatedAt: string;
+	deletedAt?: string;
 }
 
 enum AuctionStatus {
