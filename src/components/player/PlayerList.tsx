a6cdb6ce3a9e98c3c5561fc3718543d17a306663
lucianocolin <lucianocolin99@gmail.com>
2025-04-12 14:00:55 -0300

    Modify player list to handle auction creation data

--- src/components/player/PlayerList.tsx
+++ src/components/player/PlayerList.tsx
@@ -1,13 +1,69 @@
-import Card from '../ui/Card';
+import { UseMutateAsyncFunction } from '@tanstack/react-query';
 
-import { IListResponse } from '@/interfaces/api/IApiBaseResponse';
+import {
+	IListResponse,
+	ISingleResponse,
+} from '@/interfaces/api/IApiBaseResponse';
+import { ITransactionResponse } from '@/interfaces/api/ITransactionResponse';
+import { IAuction } from '@/interfaces/auction/IAuction';
+import { ICreateAuctionTransactionParams } from '@/interfaces/auction/ICreateAuctionTransaction';
+import { ISubmitCreateAuctionTransactionParams } from '@/interfaces/auction/ISubmitCreateAuction';
 import { IPlayer } from '@/interfaces/player/IPlayer';
+import PlayerCard from '@/pages/transfer-market/components/PlayerCard';
 
-const PlayerList = ({ players }: { players: IListResponse<IPlayer> }) => {
+type Props = {
+	players: IListResponse<IPlayer>;
+	createAuctionTransaction: UseMutateAsyncFunction<
+		ISingleResponse<ITransactionResponse>,
+		Error,
+		ICreateAuctionTransactionParams,
+		unknown
+	>;
+	submitCreateAuctionTransaction: UseMutateAsyncFunction<
+		ISingleResponse<IAuction>,
+		Error,
+		ISubmitCreateAuctionTransactionParams,
+		unknown
+	>;
+	handleSignTransactionXDR: (
+		transactionXDR: string,
+	) => Promise<string | undefined>;
+	createAuctionTransactionXDR:
+		| ISingleResponse<ITransactionResponse>
+		| undefined;
+	auctions: IListResponse<IAuction> | undefined;
+	isSubmittingCreateAuctionTransaction: boolean;
+	onMintPlayer: (playerId: string) => Promise<void>;
+};
+
+const PlayerList = ({
+	players,
+	createAuctionTransaction,
+	submitCreateAuctionTransaction,
+	handleSignTransactionXDR,
+	createAuctionTransactionXDR,
+	auctions,
+	isSubmittingCreateAuctionTransaction,
+	onMintPlayer,
+}: Props) => {
 	return (
-		<div className="grid grid-cols-3 gap-4 p-3">
+		<div className="grid grid-cols-3 gap-4 py-3 px-10">
 			{players?.data.map((player) => (
-				<Card key={player.id} name={player.attributes.name} />
+				<PlayerCard
+					key={player.id}
+					playerId={player.id as string}
+					playerAddress={player.attributes.address as string}
+					name={player.attributes.name}
+					createAuctionTransaction={createAuctionTransaction}
+					submitCreateAuctionTransaction={submitCreateAuctionTransaction}
+					handleSignTransactionXDR={handleSignTransactionXDR}
+					createAuctionTransactionXDR={createAuctionTransactionXDR}
+					auctions={auctions}
+					isSubmittingCreateAuctionTransaction={
+						isSubmittingCreateAuctionTransaction
+					}
+					onMintPlayer={onMintPlayer}
+				/>
 			))}
 		</div>
 	);
