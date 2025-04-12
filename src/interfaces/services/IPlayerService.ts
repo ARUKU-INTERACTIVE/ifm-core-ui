26a195405344555991c91af84a743affa3f780ab
lucianocolin <lucianocolin99@gmail.com>
2025-04-12 13:56:06 -0300

    Add methods to add player sac in service

--- src/interfaces/services/IPlayerService.ts
+++ src/interfaces/services/IPlayerService.ts
@@ -1,4 +1,5 @@
-import { IListResponse } from '../api/IApiBaseResponse';
+import { IListResponse, ISingleResponse } from '../api/IApiBaseResponse';
+import { ITransactionResponse } from '../api/ITransactionResponse';
 import { IGetAllConfig } from '../common/IGetAllConfig';
 import { IGetAllPlayersFilters } from '../player/IGetAllPlayers';
 import { IPlayer } from '../player/IPlayer';
@@ -7,4 +8,11 @@ export interface IPlayerService {
 	getAll(
 		filters: IGetAllConfig<IGetAllPlayersFilters>,
 	): Promise<IListResponse<IPlayer>>;
+	mintPlayerSac(
+		playerId: string,
+	): Promise<ISingleResponse<ITransactionResponse>>;
+	submitPlayerSac(
+		playerId: string,
+		transactionXDR: string,
+	): Promise<ISingleResponse<IPlayer>>;
 }
--- src/services/player.service.ts
+++ src/services/player.service.ts
@@ -1,6 +1,10 @@
-import { IListResponse } from './../interfaces/api/IApiBaseResponse';
+import {
+	IListResponse,
+	ISingleResponse,
+} from './../interfaces/api/IApiBaseResponse';
 import { ApiRequestConfig, apiService } from './api.service';
 
+import { ITransactionResponse } from '@/interfaces/api/ITransactionResponse';
 import { IGetAllConfig } from '@/interfaces/common/IGetAllConfig';
 import { IGetAllPlayersFilters } from '@/interfaces/player/IGetAllPlayers';
 import { IPlayer } from '@/interfaces/player/IPlayer';
@@ -29,6 +33,37 @@ class PlayerService implements IPlayerService {
 		return await this.api.get<IListResponse<IPlayer>>('/player', {
 			params: queryParams,
 		});
+	}
+
+	async mintPlayerSac(
+		playerId: string,
+	): Promise<ISingleResponse<ITransactionResponse>> {
+		try {
+			return await this.api.post<ISingleResponse<ITransactionResponse>>(
+				`/player/sac/${playerId}`,
+				{},
+			);
+		} catch (error) {
+			const err = error as Error;
+			throw new Error(err.message);
+		}
+	}
+
+	async submitPlayerSac(
+		playerId: string,
+		transactionXDR: string,
+	): Promise<ISingleResponse<IPlayer>> {
+		try {
+			return await this.api.post<ISingleResponse<IPlayer>>(
+				`/player/submit/sac/${playerId}`,
+				{
+					xdr: transactionXDR,
+				},
+			);
+		} catch (error) {
+			const err = error as Error;
+			throw new Error(err.message);
+		}
 	}
 }
 
