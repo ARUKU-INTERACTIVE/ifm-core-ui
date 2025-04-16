4a249c819fe747d96df608e183209b46ec883067
lucianocolin <lucianocolin99@gmail.com>
2025-04-16 14:24:52 -0300

    Add auction's success and error messages

--- src/interfaces/auction/auction-messages.ts
+++ src/interfaces/auction/auction-messages.ts
@@ -4,3 +4,8 @@ export const SUBMIT_CREATE_AUCTION_ERROR_MESSAGE =
 	'There was a problem at submitting the auction';
 export const CREATE_AUCTION_TRANSACTION_ERROR_MESSAGE =
 	'There was a problem at creating the auction transaction';
+export const SUBMIT_PLACE_BID_SUCCESS_MESSAGE = 'Bid submitted successfully';
+export const SUBMIT_PLACE_BID_ERROR_MESSAGE =
+	'There was an error at submitting the bid';
+export const GET_PLACE_BID_ERROR_MESSAGE =
+	'There was an error at getting the place bid transaction';
