84b916b22b713061e4a626e41929f0e8192b8adc
lucianocolin <lucianocolin99@gmail.com>
2025-04-16 14:20:51 -0300

    Add components to list auction cards

---
+++ src/pages/auction/components/AuctionCard.tsx
@@ -0,0 +1,90 @@
+import { useState } from 'react';
+
+import CreateBidModal from './CreateBidModal';
+
+import { IGetPlaceBidTransactionParams } from '@/interfaces/auction/IGetPlaceBidTransaction';
+
+interface IAuctionCardProps {
+	playerName: string;
+	highestBidAmount: string;
+	timeLeft: number;
+	isGetPlaceBidTransactionPending: boolean;
+	handleSubmitBid: (
+		getPlaceBidTransactionParams: IGetPlaceBidTransactionParams,
+	) => Promise<void>;
+	auctionId: string;
+	isSubmitPlaceBidTransactionPending: boolean;
+	myPublicKey: string;
+	highestBidderAddress: string;
+}
+
+const AuctionCard = ({
+	playerName,
+	highestBidAmount,
+	timeLeft,
+	isGetPlaceBidTransactionPending,
+	handleSubmitBid,
+	auctionId,
+	isSubmitPlaceBidTransactionPending,
+	myPublicKey,
+	highestBidderAddress,
+}: IAuctionCardProps) => {
+	const [isCreateBidModalOpen, setIsCreateBidModalOpen] =
+		useState<boolean>(false);
+
+	return (
+		<div
+			className="max-w-sm rounded overflow-hidden shadow-lg p-3 h-auto flex justify-between items-center flex-col"
+			data-test="auction-card"
+		>
+			<div>
+				<div className="text-center">
+					<div className="font-bold text-md mb-4">{playerName}</div>
+				</div>
+				<div>
+					<p className="text-gray-700 text-base">
+						<span className="font-bold">
+							Highest bid: {highestBidAmount}XLM{' '}
+						</span>{' '}
+						<br />
+						<span className="font-bold text-red-600">
+							Auction time left: {timeLeft} hours{' '}
+						</span>
+					</p>
+				</div>
+
+				{highestBidderAddress === myPublicKey && (
+					<p
+						className="text-gray-700 text-base mt-3"
+						data-test="highest-bidder-msg"
+					>
+						<span className="font-bold text-green-600">
+							{' '}
+							Congratulations! You are the highest bidder{' '}
+						</span>
+					</p>
+				)}
+			</div>
+
+			<div className="flex justify-center items-center w-full">
+				<button
+					className="bg-green-500 hover:bg-green-700 text-white font-bold py-2 px-4 rounded m-3 w-full"
+					onClick={() => setIsCreateBidModalOpen(true)}
+				>
+					Create a Bid
+				</button>
+			</div>
+
+			<CreateBidModal
+				isOpen={isCreateBidModalOpen}
+				onHide={() => setIsCreateBidModalOpen(false)}
+				isGetPlaceBidTransactionPending={isGetPlaceBidTransactionPending}
+				handleSubmitBid={handleSubmitBid}
+				auctionId={auctionId}
+				isSubmitPlaceBidTransactionPending={isSubmitPlaceBidTransactionPending}
+			/>
+		</div>
+	);
+};
+
+export default AuctionCard;
---
+++ src/pages/auction/components/AuctionList.tsx
@@ -0,0 +1,71 @@
+import AuctionCard from './AuctionCard';
+
+import {
+	IListResponse,
+	ISingleResponse,
+} from '@/interfaces/api/IApiBaseResponse';
+import { IUser } from '@/interfaces/api/IUser';
+import { IAuction } from '@/interfaces/auction/IAuction';
+import { IGetPlaceBidTransactionParams } from '@/interfaces/auction/IGetPlaceBidTransaction';
+import { IPlayer } from '@/interfaces/player/IPlayer';
+import { convertStroopsToXlm } from '@/utils/convert-stroops-to-xlm';
+import { formatPrice } from '@/utils/format-price';
+import { getAuctionTimeLeft } from '@/utils/getAuctionTimeLeft';
+
+interface AuctionListProps {
+	auctions: IListResponse<IAuction> | undefined;
+	players: IListResponse<IPlayer> | undefined;
+	isGetPlaceBidTransactionPending: boolean;
+	handleSubmitBid: (
+		getPlaceBidTransactionParams: IGetPlaceBidTransactionParams,
+	) => Promise<void>;
+	isSubmitPlaceBidTransactionPending: boolean;
+	myUser: ISingleResponse<IUser> | undefined;
+}
+
+const AuctionList = ({
+	auctions,
+	players,
+	isGetPlaceBidTransactionPending,
+	handleSubmitBid,
+	isSubmitPlaceBidTransactionPending,
+	myUser,
+}: AuctionListProps) => {
+	console.log('auctions', auctions);
+	return (
+		<div className="grid grid-cols-3 gap-4 py-3 px-10">
+			{auctions?.data.map((auction) => {
+				const auctionPlayer = players?.data.find(
+					(player) =>
+						player.attributes.address === auction.attributes.playerAddress,
+				);
+				const highestBidAmountInXLM = convertStroopsToXlm(
+					auction.attributes.highestBidAmount,
+				);
+				const auctionTimeLeft = getAuctionTimeLeft(
+					auction.attributes.startTime,
+					auction.attributes.endTime,
+				);
+
+				return (
+					<AuctionCard
+						key={auction.id}
+						playerName={auctionPlayer?.attributes.name as string}
+						highestBidAmount={formatPrice(highestBidAmountInXLM)}
+						timeLeft={auctionTimeLeft}
+						isGetPlaceBidTransactionPending={isGetPlaceBidTransactionPending}
+						handleSubmitBid={handleSubmitBid}
+						auctionId={auction.id as string}
+						isSubmitPlaceBidTransactionPending={
+							isSubmitPlaceBidTransactionPending
+						}
+						myPublicKey={myUser?.data.attributes.publicKey as string}
+						highestBidderAddress={auction.attributes.highestBidderAddress}
+					/>
+				);
+			})}
+		</div>
+	);
+};
+
+export default AuctionList;
