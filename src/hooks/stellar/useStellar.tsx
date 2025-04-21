8c7c09d4426c1f8a2275a45a98ef6e6c79e70d04
lucianocolin <lucianocolin99@gmail.com>
2025-04-21 19:33:42 -0300

    Add custom hook to handle stellar methods

---
+++ src/hooks/stellar/useStellar.tsx
@@ -0,0 +1,60 @@
+import { useState } from 'react';
+
+import {
+	CREATE_ADD_TRUSTLINE_TRANSACTION_XDR_ERROR_MESSAGE,
+	CREATE_ADD_TRUSTLINE_TRANSACTION_XDR_SUCCESS_MESSAGE,
+	SUBMIT_TRANSACTION_XDR_ERROR_MESSAGE,
+} from './stellar-messages';
+
+import { STELLAR_TOKEN_CODE } from '@/configs/environment';
+import { notificationService } from '@/services/notification.service';
+import { stellarService } from '@/services/stellar.service';
+
+export const useStellar = () => {
+	const [isLoading, setIsLoading] = useState<boolean>(false);
+	const tokenCode = STELLAR_TOKEN_CODE;
+
+	const handleCreateAddTrustlineTransactionXDR = async (
+		accountAddress: string,
+		tokenIssuer: string,
+	) => {
+		try {
+			setIsLoading(true);
+			const transactionXdr =
+				await stellarService.createAddTrustlineTransactionXDR(
+					accountAddress,
+					tokenIssuer,
+					tokenCode,
+				);
+
+			notificationService.success(
+				CREATE_ADD_TRUSTLINE_TRANSACTION_XDR_SUCCESS_MESSAGE,
+			);
+			return transactionXdr;
+		} catch (error) {
+			notificationService.error(
+				CREATE_ADD_TRUSTLINE_TRANSACTION_XDR_ERROR_MESSAGE,
+			);
+		} finally {
+			setIsLoading(false);
+		}
+	};
+
+	const handleSubmitTransactionXDR = async (xdr: string) => {
+		try {
+			setIsLoading(true);
+
+			return await stellarService.submitTransactionXDR(xdr);
+		} catch (error) {
+			notificationService.error(SUBMIT_TRANSACTION_XDR_ERROR_MESSAGE);
+		} finally {
+			setIsLoading(false);
+		}
+	};
+
+	return {
+		isLoading,
+		handleCreateAddTrustlineTransactionXDR,
+		handleSubmitTransactionXDR,
+	};
+};
