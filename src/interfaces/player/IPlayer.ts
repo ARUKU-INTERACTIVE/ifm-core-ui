cdc7270c389c15039db874a70f0318d14807a47a
ElectroCERP <carloscer27r@gmail.com>
2025-04-22 19:10:12 -0300

    Add description and imageUri props to IPlayer interface

--- src/interfaces/player/IPlayer.ts
+++ src/interfaces/player/IPlayer.ts
@@ -4,6 +4,7 @@ import { IAuction } from '../auction/IAuction';
 export interface IPlayer {
 	id: string;
 	name: string;
+	description: string;
 	uuid: string;
 	metadataUri: string;
 	issuer: string;
@@ -11,6 +12,7 @@ export interface IPlayer {
 	createdAt: string;
 	updatedAt: string;
 	deletedAt?: string;
+	imageUri?: string;
 	owner?: IUser;
 	isInAuction?: boolean;
 	auctions?: IAuction[];
