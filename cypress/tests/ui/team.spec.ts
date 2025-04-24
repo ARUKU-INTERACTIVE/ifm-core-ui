22a2fb0507799f4fda6634d39d6eaf01e09fd8c4
lucianocolin <lucianocolin99@gmail.com>
2025-04-23 21:21:18 -0300

    Add team tests and fixtures

--- cypress/tests/ui/team.spec.ts
+++ cypress/tests/ui/team.spec.ts
@@ -5,9 +5,11 @@ import {
 } from '@context/auth-messages';
 
 import {
+	ADD_PLAYER_TO_ROSTER_ERROR_MESSAGE,
 	CREATE_TEAM_ERROR_MESSAGE,
 	CREATE_TEAM_SUCCESS_MESSAGE,
 	GET_TEAM_ERROR_MESSAGE,
+	REMOVE_PLAYER_FROM_ROSTER_ERROR_MESSAGE,
 } from '@/constants/messages/team-messages';
 
 describe('Team Page', () => {
@@ -85,6 +87,13 @@ describe('Team Page', () => {
 			{ method: 'GET' },
 			{ fixture: 'user/my-user.json' },
 		);
+		cy.interceptApi(
+			'/player/sync/team',
+			{ method: 'PATCH' },
+			{
+				fixture: 'player/empty-sync-team-response.json',
+			},
+		);
 
 		cy.getBySel('team-home-msg')
 			.should('be.visible')
@@ -101,6 +110,13 @@ describe('Team Page', () => {
 			{ fixture: 'user/my-user.json' },
 		).as('get-user');
 		cy.interceptApi(
+			'/player/sync/team',
+			{ method: 'PATCH' },
+			{
+				fixture: 'player/empty-sync-team-response.json',
+			},
+		);
+		cy.interceptApi(
 			'/team',
 			{ method: 'POST' },
 			{ fixture: 'team/team-response.json' },
@@ -111,7 +127,7 @@ describe('Team Page', () => {
 			{ fixture: 'team/team-response.json' },
 		);
 		cy.interceptApi(
-			'/player?filter%5BteamId%5D=1',
+			'/player?filter%5BteamId%5D=1&page%5Bnumber%5D=1&page%5Bsize%5D=11&sort%5BcreatedAt%5D=ASC',
 			{ method: 'GET' },
 			{ fixture: 'player/team-players-response.json' },
 		);
@@ -143,6 +159,13 @@ describe('Team Page', () => {
 			{ method: 'GET' },
 			{ fixture: 'user/my-user.json' },
 		).as('get-user');
+		cy.interceptApi(
+			'/player/sync/team',
+			{ method: 'PATCH' },
+			{
+				fixture: 'player/empty-sync-team-response.json',
+			},
+		);
 		cy.interceptApi('/team', { method: 'POST' }, { statusCode: 500 });
 
 		cy.getBySel('create-team-btn').click();
@@ -161,6 +184,13 @@ describe('Team Page', () => {
 			{ fixture: 'user/my-user.json' },
 		).as('get-user');
 		cy.interceptApi(
+			'/player/sync/team',
+			{ method: 'PATCH' },
+			{
+				fixture: 'player/empty-sync-team-response.json',
+			},
+		);
+		cy.interceptApi(
 			'/team',
 			{ method: 'POST' },
 			{ fixture: 'team/team-response.json' },
@@ -171,7 +201,7 @@ describe('Team Page', () => {
 			{ fixture: 'team/team-response.json' },
 		);
 		cy.interceptApi(
-			'/player?filter%5BteamId%5D=1',
+			'/player?filter%5BteamId%5D=1&page%5Bnumber%5D=1&page%5Bsize%5D=11&sort%5BcreatedAt%5D=ASC',
 			{ method: 'GET' },
 			{ statusCode: 500 },
 		);
@@ -192,6 +222,218 @@ describe('Team Page', () => {
 		cy.wait('@get-team-user');
 		cy.getBySel('toast-container').contains(GET_TEAM_ERROR_MESSAGE);
 
-		cy.getBySel('team-players').should('exist').and('have.text', 'No players');
+		cy.getBySel('team-players-no-players')
+			.should('exist')
+			.and('have.text', 'No players in team');
+	});
+
+	it('should add a player to roster', () => {
+		cy.interceptApi(
+			'/player/sync/team',
+			{ method: 'PATCH' },
+			{
+				fixture: 'player/empty-sync-team-response.json',
+			},
+		);
+		cy.interceptApi(
+			'/user/me',
+			{ method: 'GET' },
+			{ fixture: 'user/my-team-user.json' },
+		);
+		cy.interceptApi(
+			'/team/1',
+			{ method: 'GET' },
+			{ fixture: 'team/team-with-roster-response.json' },
+		);
+		cy.interceptApi(
+			'/player?filter%5BteamId%5D=1&page%5Bnumber%5D=1&page%5Bsize%5D=11&sort%5BcreatedAt%5D=ASC',
+			{ method: 'GET' },
+			{ fixture: 'player/team-players-response.json' },
+		);
+		cy.interceptApi(
+			'/roster/1?include%5Bfields%5D=players',
+			{ method: 'GET' },
+			{ fixture: 'roster/roster-response.json' },
+		);
+
+		cy.interceptApi(
+			'/roster/add/roster/1/player/550e8400-e29b-41d4-a716-446655440001',
+			{ method: 'PATCH' },
+			{ fixture: 'player/team-players-in-roster-response.json' },
+		);
+		cy.interceptApi(
+			'/player?filter%5BteamId%5D=1&page%5Bnumber%5D=1&page%5Bsize%5D=11&sort%5BcreatedAt%5D=ASC',
+			{ method: 'GET' },
+			{ fixture: 'player/team-players-in-roster-response.json' },
+		).as('get-players-in-roster');
+		cy.interceptApi(
+			'/roster/1?include%5Bfields%5D=players',
+			{ method: 'GET' },
+			{ fixture: 'roster/roster-with-player-response.json' },
+		).as('get-roster-with-player');
+
+		cy.getBySel('card')
+			.first()
+			.then((card) => {
+				cy.wrap(card).find('button').click();
+			});
+
+		cy.wait('@get-players-in-roster');
+
+		cy.getBySel('team-roster-tab').click();
+		cy.wait('@get-roster-with-player');
+
+		cy.getBySel('card').should('be.visible').and('have.length', 1);
+	});
+
+	it('should show an error message if add player to roster fails', () => {
+		cy.interceptApi(
+			'/player/sync/team',
+			{ method: 'PATCH' },
+			{
+				fixture: 'player/empty-sync-team-response.json',
+			},
+		);
+		cy.interceptApi(
+			'/user/me',
+			{ method: 'GET' },
+			{ fixture: 'user/my-team-user.json' },
+		);
+		cy.interceptApi(
+			'/team/1',
+			{ method: 'GET' },
+			{ fixture: 'team/team-with-roster-response.json' },
+		);
+		cy.interceptApi(
+			'/player?filter%5BteamId%5D=1&page%5Bnumber%5D=1&page%5Bsize%5D=11&sort%5BcreatedAt%5D=ASC',
+			{ method: 'GET' },
+			{ fixture: 'player/team-players-response.json' },
+		);
+		cy.interceptApi(
+			'/roster/1?include%5Bfields%5D=players',
+			{ method: 'GET' },
+			{ fixture: 'roster/roster-response.json' },
+		);
+
+		cy.interceptApi(
+			'/roster/add/roster/1/player/550e8400-e29b-41d4-a716-446655440001',
+			{ method: 'PATCH' },
+			{ statusCode: 500 },
+		);
+
+		cy.getBySel('card')
+			.first()
+			.then((card) => {
+				cy.wrap(card).find('button').click();
+			});
+
+		cy.getBySel('toast-container').contains(ADD_PLAYER_TO_ROSTER_ERROR_MESSAGE);
+	});
+
+	it('should remove a player from roster', () => {
+		cy.interceptApi(
+			'/player/sync/team',
+			{ method: 'PATCH' },
+			{
+				fixture: 'player/empty-sync-team-response.json',
+			},
+		);
+		cy.interceptApi(
+			'/user/me',
+			{ method: 'GET' },
+			{ fixture: 'user/my-team-user.json' },
+		);
+		cy.interceptApi(
+			'/team/1',
+			{ method: 'GET' },
+			{ fixture: 'team/team-with-roster-response.json' },
+		);
+		cy.interceptApi(
+			'/player?filter%5BteamId%5D=1&page%5Bnumber%5D=1&page%5Bsize%5D=11&sort%5BcreatedAt%5D=ASC',
+			{ method: 'GET' },
+			{ fixture: 'player/team-players-in-roster-response.json' },
+		);
+		cy.interceptApi(
+			'/roster/1?include%5Bfields%5D=players',
+			{ method: 'GET' },
+			{ fixture: 'roster/roster-with-player-response.json' },
+		);
+
+		cy.interceptApi(
+			'/roster/remove/roster/1/player/550e8400-e29b-41d4-a716-446655440001',
+			{ method: 'PATCH' },
+			{ fixture: 'player/team-players-in-roster-response.json' },
+		);
+		cy.interceptApi(
+			'/player?filter%5BteamId%5D=1&page%5Bnumber%5D=1&page%5Bsize%5D=11&sort%5BcreatedAt%5D=ASC',
+			{ method: 'GET' },
+			{ fixture: 'player/team-players-response.json' },
+		).as('get-players');
+		cy.interceptApi(
+			'/roster/1?include%5Bfields%5D=players',
+			{ method: 'GET' },
+			{ fixture: 'roster/roster-response.json' },
+		).as('get-roster');
+
+		cy.getBySel('card')
+			.first()
+			.then((card) => {
+				cy.wrap(card).find('button').click();
+			});
+
+		cy.wait('@get-players');
+
+		cy.getBySel('team-roster-tab').click();
+		cy.wait('@get-roster');
+
+		cy.getBySel('team-roster-no-players')
+			.should('be.visible')
+			.and('have.text', 'No players in roster');
+	});
+
+	it('should show an error message if remove player from roster fails', () => {
+		cy.interceptApi(
+			'/player/sync/team',
+			{ method: 'PATCH' },
+			{
+				fixture: 'player/empty-sync-team-response.json',
+			},
+		);
+		cy.interceptApi(
+			'/user/me',
+			{ method: 'GET' },
+			{ fixture: 'user/my-team-user.json' },
+		);
+		cy.interceptApi(
+			'/team/1',
+			{ method: 'GET' },
+			{ fixture: 'team/team-with-roster-response.json' },
+		);
+		cy.interceptApi(
+			'/player?filter%5BteamId%5D=1&page%5Bnumber%5D=1&page%5Bsize%5D=11&sort%5BcreatedAt%5D=ASC',
+			{ method: 'GET' },
+			{ fixture: 'player/team-players-in-roster-response.json' },
+		);
+		cy.interceptApi(
+			'/roster/1?include%5Bfields%5D=players',
+			{ method: 'GET' },
+			{ fixture: 'roster/roster-with-player-response.json' },
+		);
+
+		cy.interceptApi(
+			'/roster/remove/roster/1/player/550e8400-e29b-41d4-a716-446655440001',
+			{ method: 'PATCH' },
+			{ statusCode: 500 },
+		);
+
+		cy.getBySel('card')
+			.first()
+			.then((card) => {
+				cy.wrap(card).find('button').click();
+			});
+
+		cy.getBySel('toast-container').contains(
+			REMOVE_PLAYER_FROM_ROSTER_ERROR_MESSAGE,
+		);
 	});
 });
