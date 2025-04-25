19bd0b63a7ab908b7cfcb820f4ee152c9a2d41ef
inadgomezdiaz <dngmdz18@gmail.com>
2025-04-25 10:40:44 -0300

    refac: remove unused authentication pages

--- src/pages/about/About.tsx
+++
@@ -1,9 +0,0 @@
-export default function About() {
-	return (
-		<div className="flex items-center justify-center h-screen flex-col">
-			<h1 className="text-3xl font-bold underline" data-test="about-msg">
-				Successfully authenticated!
-			</h1>
-		</div>
-	);
-}
