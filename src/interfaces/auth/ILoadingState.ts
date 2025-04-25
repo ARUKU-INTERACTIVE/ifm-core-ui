55188d3d6fe2afb8641b1ceaa44c70c490565c7f
inadgomezdiaz <dngmdz18@gmail.com>
2025-04-25 10:41:10 -0300

    refac: clean up authentication service by removing unused methods and messages

--- src/interfaces/auth/ILoadingState.ts
+++ src/interfaces/auth/ILoadingState.ts
@@ -1,9 +1,4 @@
 export interface ILoadingState {
 	signIn: boolean;
-	signUp: boolean;
-	forgotPassword: boolean;
-	resendConfirmationCode: boolean;
-	confirmUser: boolean;
-	confirmPassword: boolean;
 	refreshSession: boolean;
 }
