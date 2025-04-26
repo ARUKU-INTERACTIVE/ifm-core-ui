abc5ce72328f4f23a904e0826b2e07613d8c6c01
lucianocolin <lucianocolin99@gmail.com>
2025-04-25 21:21:26 -0300

    Add player position to its own component

---
+++ src/pages/formation/components/PlayerPosition.tsx
@@ -0,0 +1,60 @@
+import { IFormationSpot } from '../interfaces/coordinates.interface';
+
+export enum PlayerPositionColor {
+	YELLOW = '#FFD700',
+	BLUE = '#1E90FF',
+	RED = '#FF4500',
+	GREEN = '#4CAF50',
+	NON_SELECTED = '#808080',
+}
+
+interface IPlayerPositionProps {
+	formationSpot: IFormationSpot;
+	backgroundColor: PlayerPositionColor;
+	handleSelectSpot: () => void;
+	handleRemovePlayerFromSpot: () => void;
+	isPositionSelected: boolean;
+}
+
+const PlayerPosition = ({
+	formationSpot,
+	backgroundColor,
+	handleSelectSpot,
+	handleRemovePlayerFromSpot,
+	isPositionSelected,
+}: IPlayerPositionProps) => {
+	const { player } = formationSpot;
+	const playerInitials = player?.name
+		.split(' ')
+		.map((word) => word.charAt(0).toUpperCase())
+		.join('');
+
+	return (
+		<div
+			className="relative w-full h-6 flex items-center justify-center text-white text-xs font-bold"
+			data-test="player-position"
+		>
+			{player && (
+				<button
+					className="cursor-pointer text-xs text-red-600 absolute bottom-[100%] right-4"
+					onClick={() => handleRemovePlayerFromSpot()}
+				>
+					x
+				</button>
+			)}
+			<button
+				onClick={() => handleSelectSpot()}
+				className={`cursor-pointer w-8 h-8 rounded-full border-2 ${
+					isPositionSelected ? 'border-orange-500' : 'border-white'
+				}`}
+				style={{
+					backgroundColor: backgroundColor,
+				}}
+			>
+				{playerInitials}
+			</button>
+		</div>
+	);
+};
+
+export default PlayerPosition;
