026eba8d8eff82f49798ec9173a56dd528912f6a
ElectroCERP <carloscer27r@gmail.com>
2025-04-30 18:51:16 -0300

    Update useAuction params type

--- src/pages/transfer-market/hooks/useAuctions.ts
+++ src/pages/transfer-market/hooks/useAuctions.ts
@@ -1,9 +1,10 @@
 import { useQuery } from '@tanstack/react-query';
 
-import { IGetAllAuctionsParams } from '@/interfaces/auction/IAuction';
+import { IAuctionFilters } from '@/interfaces/auction/IAuction';
+import { IGetAllConfig } from '@/interfaces/common/IGetAllConfig';
 import { auctionService } from '@/services/auction.service';
 
-export const useAuctions = (params: IGetAllAuctionsParams) => {
+export const useAuctions = (params: IGetAllConfig<IAuctionFilters>) => {
 	return useQuery({
 		queryKey: ['auctions', params],
 		queryFn: () => auctionService.getAll(params),
